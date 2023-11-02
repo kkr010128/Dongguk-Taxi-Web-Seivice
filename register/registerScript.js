87790a433e0bcdfea3116240809ecfd185b7d7c6
kangkyuchang <mhs7465@naver.com>
2023-11-02 17:16:28 +0900

    회원가입

---
+++ register/registerScript.js
@@ -0,0 +1,6 @@
+const email_sendButton = document.getElementById("send");
+
+console.log(email_sendButton);
+// email_sendButton.addEventListener("click", function() {
+//     console.log("hi");
+// });
