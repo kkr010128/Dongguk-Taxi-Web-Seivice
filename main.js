82341fbc36bb34db830212ec14d73b0dd9e1689e
codemaker-kim <tnals72441@daum.net>
2023-11-03 01:55:39 +0900

    #18 로그인 페이지 기능 구현
    로그인 알고리즘 절반 구현
    상황에 따라 로그인 버튼 비/활성화 구현

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-/**로그인 누를 시 두근두근 분기점  TODO:로그인 데이터 DB에서 체크해서 적용하기 */
-document.addEventListener('click', function(){
-    const LoginBtn = document.getElementsById('LoginBtn');
-    const getId = document.getElementById('email');
-    const getPw = document.getElementById('password');
-    window.location.href = './main_page.html';
+const LoginBtn = document.getElementById("loginBtn");
+
+/**로그인 누를 시  */
+LoginBtn.addEventListener("click", function(){
+    var getEmail = document.getElementById("email");
+    var getPw = document.getElementById("password");
+
+    //TODO:로그인 데이터 DB에서 체크해서 적용하기
+    if(getEmail.value==getPw.value){ 
+        window.location.href = "127.0.0.1:5500"; //임시
+    }else{ //안 맞으면 경고창 띄우고 새로고침
+        alert("이메일 혹은 비밀번호를 다시 확인하세요");
+        location.replace(location.href); 
+    }
+    //console.log(getEmail.value);
 });
 
 /**회원가입 페이지로 이동시키는 함수*/
 function gotoSignUp(){
-    window.location.href="register.html"
+    window.location.href="./register/register.html";
 }
 
+/**입력값 이메일인지 체크해서 툴팁 띄우기*/
 function checkEmail() {
     var emailInput = document.querySelector('input[type="email"]');
     var tooltip = document.getElementById('tooltip');
     var pattern = /^[a-zA-Z0-9._%+-]+@dongguk\.edu$/;
 
-    if (!pattern.test(emailInput.value)) {
+    if (!pattern.test(emailInput.value)) { 
         tooltip.style.visibility = "visible";
-    } else {
-        tooltip.style.visibility = "hidden";
+
+    } else { //이메일 형식이 일치할 때만 로그인버튼 활성화
+
+        LoginBtn.style.color= "white"; // 글자색
+        var lcolor = "rgb(194, 68, 46)"; // 버튼 좌측 색깔
+        var rcolor = "rgb(255, 155, 47)"; // 버튼 우측 색깔
+        LoginBtn.style.background = `linear-gradient(to left, ${lcolor}, ${rcolor})`;
+        LoginBtn.disabled = false; //버튼 클릭 불가 해제
+        tooltip.style.visibility = "hidden"; // 툴팁 숨기기
     }
 }
 
-document.addEventListener("click",function(e){
+/*document.addEventListener("click",function(e){
     var tooltip = document.getElementById('tooltip');
     var emailInput = document.querySelector('input[type="email"]');
     if (e.target !== emailInput) {
@@ -33,4 +50,4 @@ document.addEventListener("click",function(e){
     window.location.href="./register/register.html"
 }
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-console.log(isMobile);
+console.log(isMobile);*/
