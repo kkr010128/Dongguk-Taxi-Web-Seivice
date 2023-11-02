f9d833207afb3192d4a9a6f69e53648fee7ea44e
imseokyoung <148042133+imseokyoung@users.noreply.github.com>
2023-11-02 18:42:41 +0900

    사용자 입력 저장 체크 , 아이디 말풍선 js
    사용자 입력 저장 체크 , 아이디 말풍선 자바스크립트

--- main.js
+++ main.js
@@ -2,3 +2,22 @@
 function gotoSignUp(){
     window.location.href="register.html"
 }
+function checkEmail() {
+    var emailInput = document.querySelector('input[type="email"]');
+    var tooltip = document.getElementById('tooltip');
+    var pattern = /^[a-zA-Z0-9._%+-]+@dongguk\.edu$/;
+
+    if (!pattern.test(emailInput.value)) {
+        tooltip.style.visibility = "visible";
+    } else {
+        tooltip.style.visibility = "hidden";
+    }
+}
+
+document.addEventListener("click",function(e){
+    var tooltip = document.getElementById('tooltip');
+    var emailInput = document.querySelector('input[type="email"]');
+    if (e.target !== emailInput) {
+        tooltip.style.visibility = "hidden";
+    }
+});
