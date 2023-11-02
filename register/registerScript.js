416139a930efda4d49c3d336f32563861e16baca
kangkyuchang <mhs7465@naver.com>
2023-11-02 17:51:10 +0900

    수정

--- register/registerScript.js
+++ register/registerScript.js
@@ -1,6 +1,8 @@
 const email_sendButton = document.getElementById("send");
 
-console.log(email_sendButton);
-// email_sendButton.addEventListener("click", function() {
-//     console.log("hi");
-// });
+email_sendButton.addEventListener("click", function() {
+    const email = document.querySelector("#email");
+    const register_num = document.querySelector(".register_num input");
+    register_num.display = flex;
+    console.log(register_num);
+});
