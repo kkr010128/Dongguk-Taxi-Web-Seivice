83552b66ad9bc84ce9744e2d7831e4a4ac4852b1
kangkyuchang <mhs7465@naver.com>
2023-11-02 17:18:48 +0900

    Revert "회원가입"
    This reverts commit 87790a433e0bcdfea3116240809ecfd185b7d7c6.

--- register/registerScript.js
+++
@@ -1,6 +0,0 @@
-const email_sendButton = document.getElementById("send");
-
-console.log(email_sendButton);
-// email_sendButton.addEventListener("click", function() {
-//     console.log("hi");
-// });
