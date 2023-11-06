e68062e98ec5ed1cddbe29d3d2438935da1ee7b7
codemaker-kim <tnals72441@daum.net>
2023-11-07 00:42:48 +0900

    슬라이딩 드로어 기능구현
    애니메이션 미구현
    하단 내비 비율 조정 필요

--- main_page/main_pageScript.js
+++ main_page/main_pageScript.js
@@ -1,5 +1,9 @@
-
+/**테스트 */
 const script = document.createElement('script');
+const handle = document.getElementById("handle");
+const drawer = document.getElementById("drawer");
+const okBtn = document.getElementById("setBtn");
+
 script.src = "http://dapi.kakao.com/v2/maps/sdk.js?appkey=your_app_key&autoload=false";
 script.onload = () => {
     kakao.maps.load(() => {
@@ -8,10 +12,7 @@ script.onload = () => {
 };
 document.head.appendChild(script);
 
-const handle = document.getElementById("handle");
-const drawer = document.getElementById("drawer");
-
-/**슬라이딩 드로어 이벤트 - 구현 중 */
+/**슬라이딩 드로어 이벤트*/
 handle.addEventListener('click', function(){
     if (drawer.classList.contains("drawer_open")) {
         drawer.classList.remove("drawer_open");
@@ -21,9 +22,15 @@ handle.addEventListener('click', function(){
         drawer.classList.remove("drawer_close");
         drawer.classList.add("drawer_open");
     }
-})
+});
 
-/*
+/**완료버튼 이벤트 TODO: 누를 시 매칭 + 정보를 넘겨주는 기능 추가 필요 */
+ okBtn.addEventListener('click', function(){
+    drawer.classList.remove("drawer_open");
+    drawer.classList.add("drawer_close");
+ });
+
+/*마커 테스트용
 function startMarker(){
     var marker = new kakao.maps.Marker
 }*/
